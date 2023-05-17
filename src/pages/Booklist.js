2418743a5f9bd9df95b0607ac27469654f56a957
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-05-17 16:24:09 +0300

    Route Specificity

--- src/pages/Booklist.js
+++ src/pages/Booklist.js
@@ -6,7 +6,10 @@ export function Booklist() {
         <>
         <h1 >Booklist </h1>
         <Link to= '/books/1'>Book 1</Link>
+        <br />
         <Link to= '/books/2'>Book 2</Link>
+        <br />
+        <Link to= '/books/new'>Newbook</Link>
         </>
 
     )
