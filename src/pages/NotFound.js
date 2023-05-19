13bea26378402e8421a8043fab8fb759b7fd3900
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-05-19 15:07:19 +0300

    useNavigate Hook

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,24 @@
-import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function NotFound () {
-    return <Navigate to= "/" />
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        
+        setTimeout(() =>{
+            navigate ("/")
+        }, 1000)
+
+    }, []);
+  
+    return (
+        <>
+        <h1>NotFound</h1>
+        
+        </>
+    )
 };
 
 // Navigate component is used to redirect to home page once an entered URL is not available 
+// Navigate properties are only 3.... to, replace , state
